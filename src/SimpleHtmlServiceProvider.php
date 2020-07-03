2c2a03d7ef969ead137415c58da6f365873b2f77
Ardhan <ardhan.matematika@gmail.com>
2020-07-03 19:23:25 +0700

    memperbaiki service provider

---
+++ src/SimpleHtmlServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HTML;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleHtmlServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
