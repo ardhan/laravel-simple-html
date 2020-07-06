fd41cb9993245ed2d83eb3514fdae94af4543a48
Ardhan <ardhan.matematika@gmail.com>
2020-07-07 05:10:43 +0700

    no closing meta

--- src/Meta.php
+++ src/Meta.php
@@ -9,19 +9,6 @@ class Meta extends HtmlTag
     public function __construct()
     {
         parent::__construct('meta');
-        $this->setNoClosing();
-    }
-
-
-    /**
-     * [init description]
-     * @param  string $content [description]
-     * @return [type]          [description]
-     */
-    public static function init($content = '')
-    {
-        $obj = new self;
-        $obj->name('meta')->noclosing();
-        return $obj;
+        $this->noClosing();
     }
 }
