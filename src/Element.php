86488ca5f0fa764773c385c1d16321f7ad37bd0e
Ardhan <ardhan.matematika@gmail.com>
2020-07-07 05:07:36 +0700

    menambahkan fungsi

--- src/Element.php
+++ src/Element.php
@@ -7,13 +7,10 @@ class Element
     {
     }
 
-    public function Div()
-    {
-        return new Div();
-    }
-
-    public function Span()
-    {
-        return new Span();
-    }
+    public function Div(){ return new Div(); }
+    public function Span() { return new Span(); }
+    public function Li() { return new Li(); }
+    public function Ul() { return new Ul(); }
+    public function A() { return new A(); }
+    public function H1() { return new H1(); }
 }
