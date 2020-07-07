c40d93dde065656c439c761ffdce870a6d14b25d
Ardhan <ardhan.matematika@gmail.com>
2020-07-07 21:24:12 +0700

    mencoba ubah definisi meta

--- src/Element.php
+++ src/Element.php
@@ -7,11 +7,44 @@ class Element
     {
     }
 
-    public function Div(){ return new Div(); }
+    public function Meta() {
+        $meta = new HtmlTag('meta');
+        $meta->noClosing();
+        return $meta; 
+    }
+
+    public function Div() { return new Div(); }
+
     public function Span() { return new Span(); }
+
     public function Li() { return new Li(); }
+
     public function Ul() { return new Ul(); }
+
     public function A() { return new A(); }
+
+    public function P() { return new P(); }
+
     public function H1() { return new H1(); }
-    public function Meta() { return new Meta(); }
+
+    public function H2() { return new H2(); }
+
+    public function H3() { return new H3(); }
+
+    public function H4() { return new H4(); }
+
+    public function H5() { return new H5(); }
+
+    public function H6() { return new H6(); }
+
+    public function Table() { return new Table(); }
+
+    public function Thead() { return new Thead(); }
+
+    public function Tbody() { return new Tbody(); }
+
+    public function Tr() { return new Tr(); }
+
+    public function Td() { return new Td(); }
+
 }
