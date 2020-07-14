df9804e5c8f00c112bc6228b9918ad094ddbb7d4
Ardhan <ardhan.matematika@gmail.com>
2020-07-14 14:50:00 +0700

    menambahkan kelas table

---
+++ src/Table.php
@@ -0,0 +1,17 @@
+<?php
+namespace Ardhan\LaravelSimpleHtml;
+use Ardhan\LaravelSimpleHtml\Facades\Element as El;
+
+class Table extends HtmlTag
+{
+    public function __construct()
+    {
+        
+    }
+
+
+    public function __toString()
+    {
+
+    }
+}
