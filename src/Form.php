c0d4d30283d0879be91337f30b0bcdea5a656186
Ardhan <ardhan.matematika@gmail.com>
2020-07-14 14:49:51 +0700

    art commenting

--- src/Form.php
+++ src/Form.php
@@ -4,9 +4,32 @@ use Ardhan\LaravelSimpleHtml\Facades\Element as El;
 
 class Form extends HtmlTag
 {
+    /**
+     * variable method dari form GET/POST
+     * @var string
+     */
     protected $method;
+
+
+    /**
+     * action/tujuan dari form
+     * @var string url
+     */
     protected $action;
 
+
+    /**
+     * konten dari form
+     * @var array
+     */
+    protected $formContent;
+
+
+    /**
+     * konstruksi kelas
+     * @param string $action [description]
+     * @param string $method [description]
+     */
     public function __construct($action = "", $method = "POST")
     {
         parent::__construct('form');
@@ -14,18 +37,35 @@ class Form extends HtmlTag
         $this->method = $method;
     }
 
+
+    /**
+     * mejadikan method menjadi post
+     * @return object
+     */
     public function post_method()
     {
         $this->method = "POST";
         return $this;
     }
 
+
+    /**
+     * menjadikan method menjadi get
+     * @return object
+     */
     public function get_method()
     {
         $this->method = "GET";
         return $this;
     }
 
+
+
+
+    /**
+     * [__toString description]
+     * @return string [description]
+     */
     public function __toString()
     {
         $this->attr('method', $this->method);
