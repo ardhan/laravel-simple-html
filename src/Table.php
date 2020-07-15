bbcae229737ba4353c124bd35f4b26fc279690f6
Ardhan <ardhan.matematika@gmail.com>
2020-07-15 19:31:36 +0700

    Menambahkan deklarasi untuk table

--- src/Table.php
+++ src/Table.php
@@ -4,14 +4,40 @@ use Ardhan\LaravelSimpleHtml\Facades\Element as El;
 
 class Table extends HtmlTag
 {
+    protected $row = [];
+    protected $col = [];
+    protected $cellpadding = 0;
+    protected $cellspacing = 0;
+    protected $colWidth = [];
+
     public function __construct()
     {
-        
+        $this->tag('table');
     }
 
+    public function colWidth($index, $size)
+    {
+        $this->colWidth[$index] = $size;
+    }
+
+    public function row($col)
+    {
+        $this->row[] = ["col" => $col];
+    }
+
+    public function resolveRow()
+    {
+        foreach($this->row as $row){
+            $tr = El::tr();
+            foreach($row["col"] as $col){
+                $tr->content(El::td($col));
+            }
+        }
+    }
 
     public function __toString()
     {
-
+        $this->resolveRow();
+        return parent::__toString();
     }
 }
